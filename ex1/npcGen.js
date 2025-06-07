43ec589ebb30c6a9f377ce07a5a80a8431119948
Arden Roddie <aroddie@mcad.edu>
2025-06-06 19:26:41 -0700

    First Draft completed

---
+++ ex1/npcGen.js
@@ -0,0 +1,116 @@
+// Character Options Setup
+let species = [
+  "A human",
+  "A gnome",
+  "An elf",
+  "An orc",
+  "A tiefling",
+  "A bugbear"
+];
+let profession = [
+  "baker",
+  "blacksmith",
+  "tavernkeep",
+  "fighter",
+  "wizard",
+  "ranger",
+  "bard"
+];
+let hairColor = [
+  "brown",
+  "blonde",
+  "red",
+  "black",
+  "grey",
+  "purple"
+];
+let eyeColor = [
+  "brown",
+  "blue",
+  "green",
+  "grey",
+  "black",
+  "hazel"
+];
+let pronouns = [
+  {
+    objective: "He",
+    possessive: "his"
+  },
+  {
+    objective: "She",
+    possessive: "her"
+  },
+  {
+    objective: "They",
+    possessive: "their"
+  }
+];
+let mood = [
+  "happy",
+  "sad",
+  "disgusted",
+  "grumpy",
+  "frightened",
+  "cryptic"
+];
+let weapon = [
+  "greatsword",
+  "bow and arrow",
+  "lute",
+  "greataxe",
+  "warhammer",
+  "sword and shield",
+  "spellbook",
+  "rolling pin",
+];
+
+// Global Variables
+let saveChar;
+let frameSlider;
+
+// Canvas Setup
+function setup() {
+  createCanvas(600, 400);
+  // Make a save button
+  saveChar = createButton('Save Character');
+  saveChar.mousePressed(download);
+  // Make a slider to control the frame rate
+  frameSlider = createSlider(0.5, 30, 10, 0);
+}
+
+function draw() {
+  background(220);
+  // Allow slider to control frame rate
+  let speed = frameSlider.value();
+  frameRate(speed);
+  // Show character
+  character();
+}
+
+// Create Functionality to Save Image
+function download() {
+  saveCanvas('NPC.png');
+}
+
+//Character Setup
+function character() {
+  textWrap(WORD);
+  text(randomize(species) + " " + randomize(profession) + " with " + randomize(hairColor) + " hair and " + randomize(eyeColor) + " eyes.", 50, 100);
+  // Keep pronouns consistent by randomizing before the text function
+  let gender = randomize(pronouns);
+  // Allow for proper grammar
+  if (gender.objective !== "They") {
+  text(gender.objective + " has a " + randomize(mood) + " look on " + gender.possessive + " face.", 50, 125);
+  text(gender.objective + " carries a " + randomize(weapon) + ".", 50, 150);
+  } else {
+    text(gender.objective + " have a " + randomize(mood) + " look on " + gender.possessive + " face.", 50, 125);
+    text(gender.objective + " carry a " + randomize(weapon) + ".", 50, 150);
+  }
+}
+
+// Create Randomize Function
+function randomize(array) {
+  const randomIdx = Math.floor(Math.random() * array.length);
+  return array[randomIdx];
+}
