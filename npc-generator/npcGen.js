86a48085c2e489f14b38b0c3c8af25a4ecea27d0
Arden Roddie <aroddie@mcad.edu>
2025-06-17 19:08:45 -0700

    Cleaned up backend, randomized text positions, added repository function

--- npc-generator/npcGen.js
+++ npc-generator/npcGen.js
@@ -65,33 +65,58 @@ let weapon = [
   "rolling pin",
 ];
 
-// Global Variables
+// Character Repository
+let repo = [];
+let charList;
+let listItem;
 let saveChar;
+
+// Global Variables
+let character;
+let saveImg;
+let pause;
 let frameSlider;
+let isLooping = true;
+let desc;
 
 // Canvas Setup
 function setup() {
   let canvas = createCanvas(600, 400);
-  canvas.parent("canvas-wrap")
-  // Make a save button
-  saveChar = createButton('Save Character');
-  saveChar.mousePressed(download);
-  saveChar.parent("canvas-wrap");
-  saveChar.position(50, 220);
+  canvas.parent("canvas-wrap") 
+
   // Make a slider to control the frame rate
   frameSlider = createSlider(0.5, 30, 10, 0);
   frameSlider.parent("canvas-wrap");
-  frameSlider.position(50, 20);
+  frameSlider.position(50, -20);
   frameSlider.size(500);
+
+  // Make a pause button
+  pause = createButton('Stop');
+  pause.mousePressed(togglePause);
+  pause.parent("canvas-wrap");
+  // Make a save button
+  saveChar = createButton('Save');
+  saveChar.parent("canvas-wrap");
+  //Make a download button
+  saveImg = createButton('Download');
+  saveImg.parent("canvas-wrap");
+  saveImg.mousePressed(download);
+
+  // Make the list of saved characters
+  charList = createDiv();
+  charList.parent("canvas-wrap");
+  charList.addClass("character-list");
 }
 
 function draw() {
-  background('#cfecec');
+  background('aliceblue');
   // Allow slider to control frame rate
   let speed = frameSlider.value();
   frameRate(speed);
-  // Show character
-  character();
+  // Show NPC
+  character = new NPC (species, profession, hairColor, eyeColor, pronouns, mood, weapon);
+  character.show();
+  saveChar.mousePressed(character.save);
 }
 
 // Create Functionality to Save Image
@@ -99,19 +124,50 @@ function download() {
   saveCanvas('NPC.png');
 }
 
-//Character Setup
-function character() {
-  textSize(20);
-  text(randomize(species) + " " + randomize(profession) + " with " + randomize(hairColor) + " hair and " + randomize(eyeColor) + " eyes.", 50, 100);
-  // Keep pronouns consistent by randomizing before the text function
-  let gender = randomize(pronouns);
-  // Allow for proper grammar
-  if (gender.objective !== "They") {
-  text(gender.objective + " has a " + randomize(mood) + " look on " + gender.possessive + " face.", 50, 140);
-  text(gender.objective + " carries a " + randomize(weapon) + ".", 50, 180);
+
+// Pause Functionality
+function togglePause() {
+  if ( isLooping === true) {
+        noLoop();
+        isLooping = false;
+        pause.html('Go!');
   } else {
-    text(gender.objective + " have a " + randomize(mood) + " look on " + gender.possessive + " face.", 50, 140);
-    text(gender.objective + " carry a " + randomize(weapon) + ".", 50, 180);
+      loop();
+      isLooping = true;
+      pause.html('Stop');
+  }
+}
+
+//Character Setup
+class NPC {
+  constructor(species, profession, hair, eyes, gender, mood, weapon) {
+    this.species = randomize(species);
+    this.profession = randomize(profession);
+    this.hair = randomize(hair);
+    this.eyes = randomize(eyes);
+    this.gender = randomize(gender);
+    this.mood = randomize(mood);
+    this.weapon = randomize(weapon);
+  }
+
+  show() {    
+    desc = [
+    `${this.species} ${this.profession} with ${this.hair} hair and ${this.eyes} eyes.`,
+    this.gender.objective !== 'They' ? `${this.gender.objective} has a ${this.mood} look on ${this.gender.possessive} face.` :  `${this.gender.objective} have a ${this.mood} look on ${this.gender.possessive} face.`,
+    this.gender.objective !=='They' ? `${this.gender.objective} carries a ${this.weapon}.` : `${this.gender.objective} carry a ${this.weapon}.`
+    ]
+    textSize(18);
+    textAlign(CENTER);
+    text(desc[0], random(210, width - 210), random(15, 130));
+    text(desc[1], random(160, width - 160), random(145, 260));
+    text(desc[2], random(160, width - 160), random(270, 385));
+  }
+
+  save() {
+    let npc = join(desc, " ");
+    repo.push(npc);  
+    listItem = createP(repo[repo.length -1]);
+    listItem.parent(charList);
   }
 }
 
